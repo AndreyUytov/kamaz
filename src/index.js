4362b5927a677ea685b350c92e4c69a85f3a63c0
Andrew <uytov1991@gmail.com>
2020-12-03 15:49:38 +0300

    добавить обработчики слайдера

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles/index.scss'
+
+const caruselWrapper = document.querySelector('.carusel-wrapper')
+const slider = caruselWrapper.querySelector('.main-slider')
+
+const sliderIndicators = document.querySelectorAll('.slider-idicator')
+
+const snaps = document.querySelectorAll('.main-slider__snap')
+
+let step = caruselWrapper.offsetWidth
+
+console.log(step)
