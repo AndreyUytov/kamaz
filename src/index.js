c42101050a38b8449b6ddb46be86a851e35eb5fc
Андрей <uytov1991@gmail.com>
2020-12-03 19:33:39 +0300

    закончил верстку

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import './styles/index.scss'
 
 const caruselWrapper = document.querySelector('.carusel-wrapper')
 const slider = caruselWrapper.querySelector('.main-slider')
+const controlls = document.querySelector('.main-slider__controlls')
 
 const sliderIndicators = document.querySelectorAll('.slider-idicator')
+const indicators = Array.from(sliderIndicators)
 
-const snaps = document.querySelectorAll('.main-slider__snap')
+const counter = document.querySelector('.slider-counter__item--active')
 
-let step = caruselWrapper.offsetWidth
+let currentPosition = 0
 
-console.log(step)
+controlls.addEventListener('click', (evt) => {
+  let step = caruselWrapper.getBoundingClientRect().width
+  if (
+    evt.target.closest('.main-slider__snap') ||
+    evt.target.closest('.indicator__wrapper')
+  ) {
+    if (currentPosition === 0) {
+      slider.style.marginLeft = -step + 'px'
+      indicators.forEach((elem) => {
+        elem.classList.toggle('slider-idicator--active')
+      })
+      counter.textContent = 2
+      currentPosition = -step
+    } else {
+      slider.style.marginLeft = ''
+      indicators.forEach((elem) => {
+        elem.classList.toggle('slider-idicator--active')
+      })
+      counter.textContent = 1
+      currentPosition = 0
+    }
+  }
+})
