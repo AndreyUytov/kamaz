f818c2b9107f6b63a15e26076d28a4e7f4aa267f
Andrey Shama <uytov1991@gmail.com>
2021-08-10 13:34:44 +0300

    add interactive))

--- src/index.js
+++ src/index.js
@@ -9,11 +9,42 @@ const nextButton = document.querySelector('.next')
 const markerList = document.querySelector('.slider-indicator__wrapper')
 const counter = document.querySelector('.slider-counter')
 
-new Slider({
+const carList = document.querySelector('.main-list')
+let currentCar = document.querySelector('[data-slider="0"]')
+
+let changeCurrentXCallBack = (slider) => {
+  let currentSlide = Math.floor(Math.abs(slider.currentX / slider.step))
+  let $newCar = document.querySelector(`[data-slider = "${currentSlide}"]`)
+  currentCarUpdater($newCar)
+}
+
+const slider = new Slider({
   container,
   sliderList,
   prevButton,
   nextButton,
   markerList,
   counter,
+  changeCurrentXCallBack,
 })
+
+carList.addEventListener('click', (evt) => {
+  evt.preventDefault()
+
+  let btn = evt.target.closest('[data-slider]')
+  if (!btn) return
+
+  let sliderNumber = btn.dataset.slider
+  if (!sliderNumber) return
+
+  let step = slider.step
+  slider.updateCurrentX(-step * +sliderNumber)
+
+  currentCarUpdater(btn)
+})
+
+let currentCarUpdater = ($newCar) => {
+  currentCar.classList.remove('active')
+  currentCar = $newCar
+  currentCar.classList.add('active')
+}
